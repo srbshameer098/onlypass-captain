86c0597a3ce51d2ead05ed7529d8b4c998ec0ae7
srbshameer098 <srbshameer098@gmail.com>
2024-08-03 11:42:08 +0530

    home page alignment clearing completed

--- lib/UI/View/home.dart
+++ lib/UI/View/home.dart
@@ -408,7 +408,7 @@ class _HomeState extends State<Home> {
                                 Navigator.push(
                                     context,
                                     MaterialPageRoute(
-                                        builder: (_) => Account()));
+                                        builder: (_) => const Account()));
                               },
                               child:
                                   FacilityItem(icon: icons[0], text: texts[0])),
